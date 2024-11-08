e9e90dc07d6c83e4053a6b86ab2fbc8153ce4e86
Faris Daffa <65797160+Faris0520@users.noreply.github.com>
2024-11-09 01:09:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def index():
     standings = data['data']['standings']
     return render_template('index.html', standings=standings)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
